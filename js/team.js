78b1af20af0b7b998305e5db58a9f3e5a9ff508e
Nikita Popkov <yongeryk@gmail.com>
2022-06-25 20:10:41 +0500

    Улучшил аккордеон :)

--- js/team.js
+++ js/team.js
@@ -5,20 +5,25 @@ const tabMedia = window.matchMedia('(max-width: 768px)');
 const mobMedia = window.matchMedia('(max-width: 480px)');
 
 
-
 teamList.addEventListener('click', e => {
   e.preventDefault();
-  if (e.target.classList.contains('team__show') && mobMedia.matches) {
-    clearClass('team__show--mob-active');
-    e.target.classList.add('team__show--mob-active');
-  } else if (e.target.classList.contains('team__show') && tabMedia.matches) {
-    clearClass('team__show--tab-active');
-    e.target.classList.add('team__show--tab-active');
-  } else if (e.target.classList.contains('team__show')) {
-    clearClass('team__show--active');
-    e.target.classList.add('team__show--active');
+  if (e.target.classList.contains('team__show')) {
+
+    containsClass('team__show--mob-active', e);
+    containsClass('team__show--tab-active', e);
+    containsClass('team__show--active', e);
   }
-});
+}
+);
+
+function containsClass(className, event) {
+  if (event.target.classList.contains(className))
+    event.target.classList.remove(className);
+  else {
+    clearClass(className);
+    event.target.classList.add(className);
+  }
+}
 
 function clearClass(className) {
   for (let i of document.querySelectorAll(`.${className}`))
