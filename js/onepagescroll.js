2afca0edbf5677d0fecab27b252ec06a101e424a
Nikita Popkov <yongeryk@gmail.com>
2022-07-07 20:24:53 +0500

    OnePageScroll

--- js/onepagescroll.js
+++ js/onepagescroll.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const section = $('section');
+const mainContent = $('.wrapper-content');
+let inScroll = false;
+section.first().addClass('active');
+
 $('.menu__link').on('click', function () {
   scrollTo($(this).attr('to'));
 });
@@ -11,9 +16,74 @@ $('.paginator-item').on('click', function () {
 function scrollTo(className) {
   clearClass('paginator-item--active');
   $('.paginator-item').filter((ind, el) => $(el).attr('to') === className).addClass('paginator-item--active');
-  window.scroll({
-    left: 0,
-    top: $(className).offset().top,
-    behavior: 'smooth',
+  performTransition(section.filter(className).index());
+  if (!section.first().hasClass('active')) {
+    $('.header').addClass('dn');
+  }
+  else {
+    $('.header').removeClass('dn');
+  }
+}
+
+const performTransition = sectionInd => {
+  if (!inScroll) {
+    inScroll = true;
+    const position = sectionInd * -100;
+    mainContent.css({
+      'transform': `translateY(${position}vh)`,
+    });
+    section.eq(sectionInd).addClass('active').siblings().removeClass('active');
+    setTimeout(e => {
+      inScroll = false;
+    }, 300);
+  }
+};
+
+const scrollDirection = dir => {
+  const activeSection = section.filter('.active');
+  const nextSection = activeSection.next();
+  const prevSection = activeSection.prev();
+  if (dir === 'next' && nextSection.length) {
+    scrollTo(`.${nextSection[0].classList[0]}`);
+  }
+  if (dir === 'prev' && prevSection.length) {
+    scrollTo(`.${prevSection[0].classList[0]}`);
+  }
+};
+
+$(window).on('wheel', e => {
+  const deltaY = e.originalEvent.deltaY;
+  if (deltaY > 0 && !inScroll) {
+    scrollDirection('next');
+  } else if (deltaY < 0 && !inScroll) {
+    scrollDirection('prev');
+  }
+});
+
+$(window).on('keydown', e => {
+  const next = 40;
+  const prev = 38;
+  const tagName = e.target.tagName.toLowerCase();
+
+  if (tagName !== 'input' && tagName !== 'textarea') {
+
+    if (e.keyCode == next && !inScroll) {
+      scrollDirection('next');
+    } else if (e.keyCode == prev && !inScroll) {
+      scrollDirection('prev');
+    }
+  }
+})
+
+const md = new MobileDetect();
+
+if (md.phone()) {
+  $("body").swipe({
+    swipe: function (event, direction) {
+      if (direction == 'up')
+        scrollDirection('next');
+      else if (direction == 'down')
+        scrollDirection('prev');
+    }
   });
 }
