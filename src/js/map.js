7d9807ad2475f62c1781973d41f566caca10ae2d
Nikita Popkov <yongeryk@gmail.com>
2022-07-09 03:37:21 +0500

    Фикс путей

--- src/js/map.js
+++ src/js/map.js
@@ -17,7 +17,7 @@ function init() {
   const collection = new ymaps.GeoObjectCollection({}, {
     draggable: false,
     iconLayout: 'default#image',
-    iconImageHref: '../img/marker.svg',
+    iconImageHref: './img/marker.svg',
     iconImageSize: [58, 73],
     iconImageOffset: [-29, -73],
   });
